57b5877ff38489c7a8a4eb3a099076a8a2eac277
mox <de_mox@zoho.com>
2016-04-18 17:27:51 +0200

    Added getoptpp.

--- src/doodle.cpp
+++ src/doodle.cpp
@@ -6,10 +6,68 @@
 #include "doodle_config.hpp"
 
 #include <i3ipc++/ipc.hpp>
+#include "getopt_pp.h"
 
-int  main()
+
+
+
+
+void help_message(std::string progname)
 {
+		std::cerr << "Usage: "<<progname<<" [options]"<<std::endl;
+		std::cerr << "Options:"<<std::endl;
+		std::cerr << "-s|--steps   <NUM>: Set the number of simulation steps."<<std::endl;
+		std::cerr << "-r|--radius  <NUM>: Set the radius of the stimuli."<<std::endl;
+		std::cerr << "-h|--heat    <NUM>: Set the heat of the stimuli."<<std::endl;
+}
+
+void version_message()
+{
+	std::cout<<DOODLE_PROGRAM_NAME<<":"<<std::endl;
 	std::cout<<"Git branch: "<<GIT_BRANCH<<", git commit hash: "<<GIT_COMMIT_HASH<<", Version: "<<DOODLE_VERSION_MAJOR<<":"<<DOODLE_VERSION_MINOR<<"."<<std::endl<<std::endl;
+}
+
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+	//std::cout<<"Git branch: "<<GIT_BRANCH<<", git commit hash: "<<GIT_COMMIT_HASH<<", Version: "<<DOODLE_VERSION_MAJOR<<":"<<DOODLE_VERSION_MINOR<<"."<<std::endl<<std::endl;
+	//{{{ Argument handling
+
+	bool show_help;
+	bool show_version;
+
+	GetOpt::GetOpt_pp ops(argc, argv);
+	ops.exceptions(std::ios::failbit | std::ios::eofbit);
+	try
+	{
+		//ops >> GetOpt::Option('s', "steps", steps, 100);
+		//ops >> GetOpt::Option('r', "radius", radius, 3);
+		//ops >> GetOpt::Option('h', "heat", heat, 127.0);
+		ops >> GetOpt::OptionPresent('h', "help", show_help);
+		ops >> GetOpt::OptionPresent('v', "version", show_version);
+	}
+	catch(GetOpt::GetOptEx ex)
+	{
+		std::cerr << "Error in arguments" << std::endl;
+		help_message(argv[0]);
+		return -1;
+	}
+	if(show_help)
+	{
+		help_message(argv[0]);
+		return 0;
+	}
+	if(show_version)
+	{
+		version_message();
+		return 0;
+	}
+
+	//}}}
 
 	i3ipc::I3Connection conn;
 
