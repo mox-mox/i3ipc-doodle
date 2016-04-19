c93f89fad146df09f2725f63d38704d1197f4e2e
mox <de_mox@zoho.com>
2016-04-19 13:12:45 +0200

    Trying to connect to the window change event.

--- src/doodle.cpp
+++ src/doodle.cpp
@@ -1,77 +1,45 @@
-// This is just the workplaces example from i3ipc++ that I will use as a starting point.
-// Right now, I use it to test, if I can sucessfully compile the project.
-
-
+#include "doodle.hpp"
 #include <iostream>
-#include "doodle_config.hpp"
-
-#include <i3ipc++/ipc.hpp>
-#include "getopt_pp.h"
 
 
 
 
-
-void help_message(std::string progname)
+Doodle::Doodle()
 {
-		std::cerr << "Usage: "<<progname<<" [options]"<<std::endl;
-		std::cerr << "Options:"<<std::endl;
-		std::cerr << "-s|--steps   <NUM>: Set the number of simulation steps."<<std::endl;
-		std::cerr << "-r|--radius  <NUM>: Set the radius of the stimuli."<<std::endl;
-		std::cerr << "-h|--heat    <NUM>: Set the heat of the stimuli."<<std::endl;
+	conn = new i3ipc::I3Connection();
 }
 
-void version_message()
+Doodle::~Doodle()
 {
-	std::cout<<DOODLE_PROGRAM_NAME<<":"<<std::endl;
-	std::cout<<"Git branch: "<<GIT_BRANCH<<", git commit hash: "<<GIT_COMMIT_HASH<<", Version: "<<DOODLE_VERSION_MAJOR<<":"<<DOODLE_VERSION_MINOR<<"."<<std::endl<<std::endl;
+	delete conn;
 }
 
 
-
-
-
-
-int main(int argc, char* argv[])
+void Doodle::subscribe_to_window_change()
 {
-	//std::cout<<"Git branch: "<<GIT_BRANCH<<", git commit hash: "<<GIT_COMMIT_HASH<<", Version: "<<DOODLE_VERSION_MAJOR<<":"<<DOODLE_VERSION_MINOR<<"."<<std::endl<<std::endl;
-	//{{{ Argument handling
+	conn->subscribe(i3ipc::ET_WINDOW);
+	//mem_functor0<T_return, T_obj> sigc::mem_fun 	( 	T_return(T_obj::*)()  	_A_func	)
+	//mem_functor1<T_return, T_obj, T_arg1> sigc::mem_fun 	( 	T_return(T_obj::*)(T_arg1)  	_A_func	)
+	//sigc::mem_fun( 	T_return(T_obj::*)()  	_A_func	)
+	//sigc::mem_fun( 	T_return(T_obj::*)(T_arg1)  	_A_func	)
+	//conn->signal_window_event.connect(sigc::mem_fun(*this, &Doodle::on_window_change));
+	conn->signal_output_event.connect(sigc::mem_fun(*this, &Doodle::on_window_change));
+}
 
-	bool show_help;
-	bool show_version;
+//void Doodle::on_window_change(WindowEventType win_evt)
+void Doodle::on_window_change()
+{
+	std::cout<<"on_window_change() called."<<std::endl;
 
-	GetOpt::GetOpt_pp ops(argc, argv);
-	ops.exceptions(std::ios::failbit | std::ios::eofbit);
-	try
-	{
-		//ops >> GetOpt::Option('s', "steps", steps, 100);
-		//ops >> GetOpt::Option('r', "radius", radius, 3);
-		//ops >> GetOpt::Option('h', "heat", heat, 127.0);
-		ops >> GetOpt::OptionPresent('h', "help", show_help);
-		ops >> GetOpt::OptionPresent('v', "version", show_version);
-	}
-	catch(GetOpt::GetOptEx ex)
-	{
-		std::cerr << "Error in arguments" << std::endl;
-		help_message(argv[0]);
-		return -1;
-	}
-	if(show_help)
-	{
-		help_message(argv[0]);
-		return 0;
-	}
-	if(show_version)
-	{
-		version_message();
-		return 0;
-	}
 
-	//}}}
+}
 
-	i3ipc::I3Connection conn;
 
-	for( auto&  w : conn.get_workspaces())
+
+
+void Doodle::print_workspaces()
+{
+	for( auto&  w : conn->get_workspaces())
 	{
 		std::cout<<'#'<<std::hex<<w.num<<std::dec
 		         <<"\n\tName: "<<w.name
@@ -86,5 +54,4 @@ int main(int argc, char* argv[])
 		         <<"\n\tOutput: "<<w.output
 		         <<std::endl;
 	}
-	return 0;
 }
