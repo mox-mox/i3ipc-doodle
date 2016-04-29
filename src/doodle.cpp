bfcb1bb2278983f670148a10966d6903fc152043
mox <de_mox@zoho.com>
2016-04-29 21:15:58 +0200

    Starting window -> Job matching.

--- src/doodle.cpp
+++ src/doodle.cpp
@@ -4,8 +4,29 @@
 
 
 
-Doodle::Doodle(i3ipc::connection& conn) : conn(conn), count(0)
+Doodle::Doodle(i3ipc::connection& conn) : conn(conn), evt_count(0)
 {
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wpedantic"
+	jobs = {
+		{
+			.jobname = "project",
+			.times = {},
+			.window_name_segments = { "/home/mox/projects", "zathura" }
+		},
+		{
+			.jobname = "scratch",
+			.times = {},
+			.window_name_segments = { "/home/mox/scratch", "okular" }
+		}
+	};
+
+			for( Job& j : jobs)
+			{
+				std::cout<<"Address of "<<j.jobname<<" is "<<&j<<"."<<std::endl;
+			}
+	#pragma GCC diagnostic pop
+
 	conn.signal_window_event.connect(sigc::mem_fun(*this, &Doodle::on_window_change));
 	conn.signal_workspace_event.connect(sigc::mem_fun(*this, &Doodle::on_workspace_change));
 	if(!conn.subscribe(i3ipc::ET_WORKSPACE | i3ipc::ET_WINDOW))
@@ -19,48 +40,120 @@ Doodle::Doodle(i3ipc::connection& conn) : conn(conn), count(0)
 }
 
 
+inline Doodle::Job* Doodle::find_job(std::string window_name)
+{
+	Job* retval = nullptr;
+	for( Job& j : jobs)											// Search all the jobs to see, ...
+	{
+		for( std::string name_segment : j.window_name_segments)	// ... if one has a matching name segment.
+		{
+			bool exclude = false;
+			if('!' == name_segment[0])	// If the name segment is prepended with a '!', finding the name segment
+			{							// in the window name means that the window does NOT belong to the job.
+				exclude = true;
+				name_segment.erase(0, 1);
+			}
+
+			if(std::string::npos != window_name.find(name_segment))
+			{
+				if(exclude)	// The window does not belong to the job.
+				{
+					break;	// out of the name_segment loop -> go to the next job.
+				}
+				else		// The window belongs to the job.
+				{
+					std::cout<<"Window matched job "<<j.jobname<<", matching name segment: "<<name_segment<<". Address:"<<&j<<std::endl;
+					#ifndef DEBUG	// For normal operation, just report the first match.
+					retval = &j;
+					return retval;
+					#else			// When debugging, continue searching to see if there are other matches
+					if(retval)		// e.g. if there is ambiguity in the window_name_segments.
+					{
+						std::cerr<<"(EE): Window name matched "<<retval->jobname<<" and "<<j.jobname<<"."<<std::endl;
+					}
+					else
+					{
+						retval = &j;
+					}
+					#endif
+				}
+			}
+
+		}
+
+	}
+	return retval;
+}
+
+
+
+
+
 void Doodle::on_window_change(const i3ipc::window_event_t& evt)
 {
-	std::cout<<"(LL): on_window_change() called "<<++count<<"th time."<<std::endl;
+	std::cout<<"(LL): on_window_change() called "<<++evt_count<<"th time."<<std::endl;
 	std::cout<<"	type: "<<static_cast<char>(evt.type)<<std::endl;
 	if(evt.container!=nullptr)
 	{
 		std::cout<<"	id = "<<evt.container->id<<std::endl;
-		std::cout<<"	xwindow_id = "<<evt.container->xwindow_id<<std::endl;
+		//std::cout<<"	xwindow_id = "<<evt.container->xwindow_id<<std::endl;
 		std::cout<<"	name = "<<evt.container->name<<std::endl;
 		std::cout<<"	type = "<<evt.container->type<<std::endl;
 		std::cout<<"	border = "<<static_cast<char>(evt.container->border)<<std::endl;
-		std::cout<<"	border_raw = "<<evt.container->border_raw<<std::endl;
-		std::cout<<"	current_border_width = "<<evt.container->current_border_width<<std::endl;
-		std::cout<<"	layout = "<<static_cast<char>(evt.container->layout)<<std::endl;
-		std::cout<<"	layout_raw = "<<evt.container->layout_raw<<std::endl;
-		std::cout<<"	percent = "<<evt.container->percent<<std::endl;
-		std::cout<<"	rect.x = "<<evt.container->rect.x<<std::endl;
-		std::cout<<"	rect.y = "<<evt.container->rect.y<<std::endl;
-		std::cout<<"	rect.width = "<<evt.container->rect.width<<std::endl;
-		std::cout<<"	rect.height = "<<evt.container->rect.height<<std::endl;
-		std::cout<<"	window_rect.x = "<<evt.container->window_rect.x<<std::endl;
-		std::cout<<"	window_rect.y = "<<evt.container->window_rect.y<<std::endl;
-		std::cout<<"	window_rect.width = "<<evt.container->window_rect.width<<std::endl;
-		std::cout<<"	window_rect.height = "<<evt.container->window_rect.height<<std::endl;
-		std::cout<<"	deco_rect.x = "<<evt.container->deco_rect.x<<std::endl;
-		std::cout<<"	deco_rect.y = "<<evt.container->deco_rect.y<<std::endl;
-		std::cout<<"	deco_rect.width = "<<evt.container->deco_rect.width<<std::endl;
-		std::cout<<"	deco_rect.height = "<<evt.container->deco_rect.height<<std::endl;
-		std::cout<<"	geometry.x = "<<evt.container->geometry.x<<std::endl;
-		std::cout<<"	geometry.y = "<<evt.container->geometry.y<<std::endl;
-		std::cout<<"	geometry.width = "<<evt.container->geometry.width<<std::endl;
-		std::cout<<"	geometry.height = "<<evt.container->geometry.height<<std::endl;
+		//std::cout<<"	border_raw = "<<evt.container->border_raw<<std::endl;
+		//std::cout<<"	current_border_width = "<<evt.container->current_border_width<<std::endl;
+		//std::cout<<"	layout = "<<static_cast<char>(evt.container->layout)<<std::endl;
+		//std::cout<<"	layout_raw = "<<evt.container->layout_raw<<std::endl;
+		//std::cout<<"	percent = "<<evt.container->percent<<std::endl;
+		//std::cout<<"	rect.x = "<<evt.container->rect.x<<std::endl;
+		//std::cout<<"	rect.y = "<<evt.container->rect.y<<std::endl;
+		//std::cout<<"	rect.width = "<<evt.container->rect.width<<std::endl;
+		//std::cout<<"	rect.height = "<<evt.container->rect.height<<std::endl;
+		//std::cout<<"	window_rect.x = "<<evt.container->window_rect.x<<std::endl;
+		//std::cout<<"	window_rect.y = "<<evt.container->window_rect.y<<std::endl;
+		//std::cout<<"	window_rect.width = "<<evt.container->window_rect.width<<std::endl;
+		//std::cout<<"	window_rect.height = "<<evt.container->window_rect.height<<std::endl;
+		//std::cout<<"	deco_rect.x = "<<evt.container->deco_rect.x<<std::endl;
+		//std::cout<<"	deco_rect.y = "<<evt.container->deco_rect.y<<std::endl;
+		//std::cout<<"	deco_rect.width = "<<evt.container->deco_rect.width<<std::endl;
+		//std::cout<<"	deco_rect.height = "<<evt.container->deco_rect.height<<std::endl;
+		//std::cout<<"	geometry.x = "<<evt.container->geometry.x<<std::endl;
+		//std::cout<<"	geometry.y = "<<evt.container->geometry.y<<std::endl;
+		//std::cout<<"	geometry.width = "<<evt.container->geometry.width<<std::endl;
+		//std::cout<<"	geometry.height = "<<evt.container->geometry.height<<std::endl;
 		std::cout<<"	urgent = "<<evt.container->urgent<<std::endl;
 		std::cout<<"	focused = "<<evt.container->focused<<std::endl;
+	}
+	if((evt.type == i3ipc::WindowEventType::FOCUS || evt.type == i3ipc::WindowEventType::TITLE) && evt.container!=nullptr)
+	{
+		Job*& job = win_id_lookup[evt.container->id];
+		if(!job) // Window not yet associated with a job
+		{
+			job = find_job(evt.container->name);
+		}
+		else
+		{
+			std::cout<<"Job found in map."<<std::endl;
+		}
 
 	}
+	else if(evt.type == i3ipc::WindowEventType::CLOSE)
+	{
+		win_id_lookup.erase(evt.container->id);
+	}
 
+
+	for (auto it : win_id_lookup)
+	{
+		std::cout << "first: "<<it.first << ", second: " << it.second << '\n';
+	}
 }
+
+
 
 void Doodle::on_workspace_change(const i3ipc::workspace_event_t&  evt)
 {
-	std::cout<<"(LL): on_workspace_change() called "<<++count<<"th time."<<std::endl;
+	std::cout<<"(LL): on_workspace_change() called "<<++evt_count<<"th time."<<std::endl;
 }
 
 
--- src/doodle.hpp
+++ src/doodle.hpp
@@ -1,12 +1,59 @@
 #pragma once
 #include <i3ipc++/ipc.hpp>
+#include <map>
+#include <deque>
+#include <chrono>
 //#include <json/json.h>
+
+using window_id = uint64_t;
 
 class Doodle : public sigc::trackable
 {
 	private:
 		i3ipc::connection& conn;
-		int count;
+		std::string current_ws_name;
+		int evt_count;
+
+
+		struct Job
+		{
+			std::string jobname;
+			// Accounting
+			//{{{
+			class Timespan
+			{
+				public:
+					Timespan(std::chrono::system_clock::time_point start, std::chrono::system_clock::time_point end) : start(start), end(end)
+					{
+						#ifdef DEBUG
+						assert(start < end);
+						#endif
+					}
+					operator std::chrono::duration<double>()
+					{
+						std::chrono::duration<double> diff = end-start;
+						return diff;
+					}
+
+				private:
+					std::chrono::system_clock::time_point start;
+					std::chrono::system_clock::time_point end;
+			};
+			//}}}
+
+			// Time-keeping
+			std::deque<Timespan> times;
+
+			// Matching
+			std::deque<std::string> window_name_segments;	// Window name segments prefixed by "!" are excluded. Note: Group exclude names first.
+			//std::deque<std::string> workspaces;			// Prefix by "!" to exclude specific workspaces
+		};
+
+		// Window recognition
+		std::map<window_id, Job*> win_id_lookup;
+		std::deque<Job> jobs;
+
+		inline Job* find_job(std::string window_name);
 
 	public:
 		Doodle(i3ipc::connection& conn);
