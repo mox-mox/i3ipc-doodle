92cff6c0dc62b3572478f3dd136a8be0259e687e
mox <de_mox@zoho.com>
2017-12-04 00:30:46 +0100

    Can listen to the pipe. TODO: Feed that into i3ipcpp

--- daemon/doodle.cpp
+++ daemon/doodle.cpp
@@ -1,6 +1,7 @@
 #include <doodle.hpp>
 #include "main.hpp"
 
+#include <uvw.hpp>
 
 
 //{{{
@@ -52,12 +53,77 @@ int Doodle::operator()(void)
 
 	logger<<"---------------Starting the event loop---------------"<<std::endl;
 
-	while( true )
-	{
-		std::cout<<"7"<<std::endl;
-		i3_conn.handle_event();
-		std::cout<<"8"<<std::endl;
-	}
+	auto loop = uvw::Loop::getDefault();
+
+	// Create a resource that will listen to STDIN
+    auto console = loop->resource<uvw::TTYHandle>(uvw::StdIN, true);
+    console->on<uvw::DataEvent>([](auto& evt, auto& hndl){
+			(void) hndl;
+			std::cout<<"Got something from STDIN: "<<std::endl;
+			std::cout<<'	'<<std::string(&evt.data[0], evt.length)<<std::endl;
+    });
+	console->on<uvw::CloseEvent>([](const uvw::CloseEvent &, uvw::TTYHandle &) { std::cout<<"TTY close"<<std::endl; });
+	console->read();
+
+
+
+
+
+
+
+
+
+
+	//{{{ Listen to the i3 pipe
+
+    auto pipe = loop->resource<uvw::PipeHandle>();
+	pipe->init();
+    //pipe->on<uvw::WriteEvent>([](const uvw::WriteEvent &, uvw::PipeHandle &) {
+	//		//handle.close();
+	//		std::cout<<"Wrote to the pipe"<<std::endl;
+    //});
+
+    //pipe->on<uvw::ConnectEvent>([](const uvw::ConnectEvent &, uvw::PipeHandle &handle) {
+    //    auto dataTryWrite = std::unique_ptr<char[]>(new char[1]{ 'a' });
+    //    handle.tryWrite(std::move(dataTryWrite), 1);
+    //    auto dataWrite = std::unique_ptr<char[]>(new char[2]{ 'b', 'c' });
+    //    handle.write(std::move(dataWrite), 2);
+    //});
+	//pipe->on<uvw::CloseEvent>([&loop](const uvw::CloseEvent &, uvw::PipeHandle &) {
+	//		loop->walk([](uvw::BaseHandle &h){ h.close(); });
+	//		std::cout<<"Server close"<<std::endl;
+	//});
+	//pipe->on<uvw::EndEvent>([](const uvw::EndEvent &, uvw::PipeHandle &sock) {
+	//		sock.close();
+	//		std::cout<<"Socket close"<<std::endl;
+	//});
+	
+    pipe->on<uvw::DataEvent>([](const uvw::DataEvent& evt, auto&){
+			std::cout<<"Got something from the pipe: "<<std::endl;
+			std::cout<<'	'<<std::string(&evt.data[0], evt.length)<<std::endl;
+    });
+
+	//pipe->connect(socket_path);
+	pipe->open(i3_conn.get_event_socket_fd());
+
+	pipe->read();
+
+
+
+	//}}}
+
+    loop->run();
+	loop->walk([](uvw::BaseHandle &h){ h.close(); });
+
+
+
+
+	//while( true )
+	//{
+	//	std::cout<<"7"<<std::endl;
+	//	i3_conn.handle_event();
+	//	std::cout<<"8"<<std::endl;
+	//}
 
 	logger<<"Returning from event loop"<<std::endl;
 
