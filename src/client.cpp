0bda4ac76e564de232f0aaabe7c4e4896339459a
mox <de_mox@zoho.com>
2017-12-12 00:09:12 +0100

    Not working, researching REPLs

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,27 @@
 #include <client.hpp>
 #include "main.hpp"
 #include "sockets.hpp"
+#include <stropts.h>
+#include <sstream>
+
+
+// EDITLINE
+
+//     el_set()
+//           Set editline parameters.  op determines which parameter to set, and each operation has its own parameter
+//           list.  Returns 0 on success, -1 on failure.
+//           EL_UNBUFFERED, int flag
+//                 If flag is zero, unbuffered mode is disabled (the default).  In unbuffered mode, el_gets() will
+//                 return immediately after processing a single character.
+
+
+//{{{
+const char* prompt(EditLine* e)
+{
+	(void) e;
+	return "doodle > ";
+}
+//}}}
 
 
 //{{{
@@ -8,8 +29,48 @@ Client::Client(void) :
 	loop(uvw::Loop::getDefault()),
 	sigint(loop->resource<uvw::SignalHandle>()),
 	daemon_pipe(loop->resource<uvw::PipeHandle>()),
+	//console(loop->resource<uvw::IdleHandle>())
 	console(loop->resource<uvw::TTYHandle>(uvw::StdIN, true))
 {
+
+	//{{{
+	el = el_init(DOODLE_PROGRAM_NAME, stdin, stdout, stderr);
+	if(!el) throw std::logic_error("no el");
+	el_set(el, EL_PROMPT, prompt);
+	el_set(el, EL_EDITOR, "vi");
+
+	/* Initialize the history */
+	myhistory = history_init();
+	if (myhistory == nullptr)
+	{
+		throw std::runtime_error("History could not be initialised.");
+	}
+
+	/* Set the size of the history */
+	history(myhistory, &hist_ev, H_SETSIZE, 800);
+
+	//for(auto& command : commands)
+	//{
+	//	history(myhistory, &hist_ev, H_ENTER, (command.first + " " + command.second.args).c_str());
+	//}
+
+
+	/* This sets up the call back functions for history functionality */
+	el_set(el, EL_HIST, history, myhistory);
+
+
+
+	/* Make editline return after each character */
+	el_set(el, EL_UNBUFFERED, 1);
+
+	//}}}
+
+
+
+
+
+
+
 	//{{{ Initialise all event watchers
 	
 	//{{{ <Ctrl+c>/SIGINT watcher
@@ -21,8 +82,7 @@ Client::Client(void) :
 	sigint->start(SIGINT);
 	//}}}
 
-	//{{{
-	// Create a resource that will listen to STDIN
+	//{{{ Daemon pipe
 
 	daemon_pipe->init();
     daemon_pipe->on<uvw::WriteEvent>([](const uvw::WriteEvent &, uvw::PipeHandle &) {
@@ -55,17 +115,122 @@ Client::Client(void) :
 	daemon_pipe->read();
 	//}}}
 
-	//{{{ Create a resource that will listen to STDIN
+//
+//	//{{{ Console watcher
+//
+//    console->on<uvw::IdleEvent>([this](auto&, auto&){
+//			//std::cout<<"Running IdleEvent"<<std::endl;
+//			int n;
+//
+//			sleep(1);
+//			std::cout<<"------------------------"<<std::endl;
+//			if (ioctl(0, I_NREAD, &n) == 0 && n > 0)
+//			{
+//				std::cout<<"AVAILABLE"<<std::endl;
+//
+//				char c;
+//				std::cin>>c;
+//				std::cout<<"Got \""<<c<<"\"."<<std::endl;
+//			}
+//
+//
+//			//if (!feof(stdin)) // Check if the stdin is empty
+//			//{
+//			//	std::cout<<"-"<<std::endl;
+//			//	line = el_gets(el, &count);
+//			//	std::cout<<"line = "<<line<<std::endl;
+//			//}
+//
+//			//if (count > 0)
+//			//{
+//			//history(myhistory, &hist_ev, H_ENTER, line);
+//			////sock.send(parse_command({line, static_cast<unsigned int>(count)}));
+//			//std::cout<<(parse_command({line, static_cast<unsigned int>(count)}))<<std::endl;
+//			//}
+//			//else
+//			//{
+//			//std::cout<<"Invalid entry."<<std::endl;
+//			//}
+//			////return *this;
+//    });
+//	console->start();
+//	//}}}
+//
 
-    console->on<uvw::DataEvent>([this](auto& evt, auto& hndl){
-			(void) hndl;
-			std::cout<<"Got something from STDIN: "<<std::endl;
-			std::cout<<'	'<<std::string(&evt.data[0], evt.length)<<std::endl;
-			daemon_pipe->write(&evt.data[0], evt.length);
+
+	//{{{ Console watcher
+
+    console->on<uvw::DataEvent>([this](auto& evt, auto&){
+			//(void) hndl;
+			//std::cout<<"Got something from STDIN: "<<std::endl;
+			//std::cout<<'	'<<std::string(&evt.data[0], evt.length)<<std::endl;
+			//daemon_pipe->write(&evt.data[0], evt.length);
+
+
+			std::cout<<"------------------------"<<std::endl;
+			//if (int n = 0; ioctl(0, I_NREAD, &n) == 0 && n > 0)
+
+			{
+			//line = el_gets(el, &count);
+			//std::cout<<"line = "<<line<<std::endl;
+
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+			//{{{
+
+			// Prepare fake stringstream
+			std::stringstream fake_cin;
+			//fake_cin << "Hi";
+			fake_cin.write(&evt.data[0], evt.length);
+
+			// Backup and change std::cin's streambuffer
+			std::streambuf *backup = std::cin.rdbuf(); // back up cin's streambuf
+			std::streambuf *psbuf = fake_cin.rdbuf(); // get file's streambuf
+			std::cin.rdbuf(psbuf); // assign streambuf to cin
+
+			// Read something, will come from out stringstream
+			//std::string input;
+			//std::cin >> input;
+
+			line = el_gets(el, &count);
+			std::cout<<"line = "<<line<<std::endl;
+
+
+
+
+
+
+			// Verify that we actually read the right text
+			//std::cout << input << std::endl;
+
+			// Restore old situation
+			std::cin.rdbuf(backup); // restore cin's original streambuf
+			//}}}
+
+
+
+
+
+
+
     });
-	console->on<uvw::CloseEvent>([](const uvw::CloseEvent &, uvw::TTYHandle &) { std::cout<<"TTY close"<<std::endl; });
+	console->on<uvw::CloseEvent>([](const uvw::CloseEvent&, uvw::TTYHandle& console) { console.reset(), std::cout<<"TTY close"<<std::endl; });
+	console->mode(uvw::details::UVTTYModeT::RAW);
 	console->read();
 	//}}}
+
+
 	//}}}
 }
 //}}}
--- src/client.hpp
+++ src/client.hpp
@@ -1,16 +1,62 @@
 #pragma once
-
-
-//#include <i3ipc++/ipc.hpp>
 #include <uvw.hpp>
+
+
+
+//{{{ Redirect the read() function to read from an std::string called "stdin_buffer" instead
+
+std::string stdin_buffer;
+ssize_t read_stdin_from_string(int fd, void* buf, size_t count)
+{
+	if( fd != 0 )
+	{
+		return read(fd, buf, count);
+	}
+	else
+	{
+		size_t n =  count < stdin_buffer.length() ? count : stdin_buffer.length();
+		std::strncpy(reinterpret_cast<char*>(buf), stdin_buffer.c_str(), n);
+		stdin_buffer.erase(0, n);
+		return n;
+	}
+}
+//}}}
+
+#define read read_stdin_from_string
+// Take care to ever only include the editline functions in one place
+// TODO: verify if this also works with a dynamic library
+#include <histedit.h>
+#undef read
+
+
 
 class Client
 {
-	std::shared_ptr<uvw::Loop> loop;
+	//{{{ Event handling
 
+	std::shared_ptr<uvw::Loop> loop;
 	std::shared_ptr<uvw::SignalHandle> sigint;
 	std::shared_ptr<uvw::PipeHandle> daemon_pipe;
+	//std::shared_ptr<uvw::IdleHandle> console;
 	std::shared_ptr<uvw::TTYHandle> console;
+	//}}}
+
+
+
+
+	//{{{ Editline
+	int count;
+	const char* line;
+
+	EditLine* el;
+	HistEvent hist_ev;
+	History *myhistory;
+	//}}}
+
+	std::string parse_command(std::string entry);
+	std::string parse_response(std::string response);
+
+
 
 	public:
 		//{{{ Constructor
